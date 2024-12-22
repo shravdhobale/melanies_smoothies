51564d8cb6ac37f083db228bff729f4906954290
shravani <164045795+shravdhobale@users.noreply.github.com>
2024-12-23 00:21:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
