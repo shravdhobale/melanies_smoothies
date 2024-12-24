d9a1d694461c2cc402c8f02449bf3880b4415eef
shravani <164045795+shravdhobale@users.noreply.github.com>
2024-12-24 12:16:02 +0530

    Update streamlit_app.py
    Imported get_acitve_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark.session import get_active_session
 
 
 # Write directly to the app
